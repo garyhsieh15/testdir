8b704b4482afe4f89c680fd8cc3107dd22ec65ff
garyhsieh <garyhsieh@garyhsiehs-iMac.AirDream>
2020-07-22 01:50:25 +0800

    test03 github

--- line_plot.py
+++ line_plot.py
@@ -23,6 +23,7 @@ def main():
     print("test github status")
     print("test01 github status")
     print("test02 gighub status")
+    print("test03 github status")
 
 if __name__ == "__main__":
     main()

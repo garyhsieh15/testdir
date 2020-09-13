ff9d1c8e91d39f7682c6d360355b82f07c3f5553
garyhsieh <garyhsieh@garyhsiehs-iMac.AirDream>
2020-09-14 01:10:54 +0800

    modify python_math.py and score.csv

--- python_math.py
+++ python_math.py
@@ -1,14 +1,18 @@
 # import matplotlib.pyplot as plt
 # import numpy as np
 
-import test3_4
+# from sympy import Symbol, solve
+
+# import test3_4
 
 import draw_circle
 import carlo
+import cal_AMM
 
 if __name__ == "__main__":
     
     # test3_4.solve_a_b()
     # draw_circle.draw_circle()
 
-    carlo.circle()    
+    # carlo.circle()    
+    # cal_AMM.get_AMM()

4105bff8dbaa746c2d411132c24af252eb090324
garyhsieh <garyhsieh@garyhsiehs-iMac.AirDream>
2020-12-14 01:35:48 +0800

    =modify draw_func.py

--- dynamic/draw_func.py
+++ dynamic/draw_func.py
@@ -62,10 +62,19 @@ def all_theo_sol():
     # jump 1
     td_div_Tn = [1/8, 1/4, 1/2, 3/4, 0.9999, 1.5, 2, 2.5, 3, 3.5, 4, 5]
     time_slice = 100
+    umax = []
+    umax_free = []
+    Rd = []
+    tndtn = []
     for i in td_div_Tn:
         u = []
         ufree = []
         ust = []
+
+        _umax = 0
+        _umax_free = 0
+        umax.append(str(i))
+        umax_free.append(str(i))
         for j in np.arange(0, i + i/time_slice, i/time_slice):
             # j = t/Tn
             # tndt = Tn_div_td
@@ -83,6 +92,12 @@ def all_theo_sol():
         for l in np.arange(0, i * 2, i * 2 / time_slice):
             yst = math.sin( 2 * pi * (1 / i) * l)
             ust.append(yst)
+        
+        Rd.append(max(u + ufree))
+        tndtn.append(1/i)
+
+        umax.append(max(u))
+        umax_free.append(max(ufree))
 
         plt.figure(figsize = (20, 6))
         plt.plot(np.arange(0, i + i/time_slice, i/time_slice), u, label = "forced, td/Tn = " + str(i))
@@ -94,8 +109,33 @@ def all_theo_sol():
         plt.ylabel("u(t)/(ust)0")
 
     plt.show()
+    
+    print("max of u(t) during forced:")
+    print("%8s %8s" % ("td/Tn", "u0"))
+    print(" ------------------------------------------- ")
+    for i in range(0, len(umax), +2):
+      #print(umax[i], "    ", umax[i + 1])
+      print("%8s %8.4f" % (umax[i], umax[i + 1]))
 
+    print("max of u(t) during free:")
+    print("%8s %8s" % ("td/Tn", "u0"))
+    print(" ------------------------------------------- ")
+    for i in range(0, len(umax_free), +2):
+        print("%8s %8.4f" % (umax_free[i], umax_free[i + 1]))
+
+    #print("show Rd valeu: ", Rd)
+    #print("max of u(t) during forced: ", umax)
+    #print("max of u(t) during free: ", umax_free)
+    plt.figure(figsize = (20, 6))
+    plt.plot(tndtn, Rd, label = "Rd - W/Wn")
+    plt.grid(True)
+    plt.legend()
+    plt.xlabel("W/Wn")
+    plt.ylabel("Rd")
+
+    plt.show()
 
 if __name__ == "__main__":
-    theo_sol()
-    #all_theo_sol()
+    #theo_sol()
+    all_theo_sol()
+
